5c272f1881439810c6d82d1637c35b40576c6317
Subhankar Mandal <subhankarmandal875@gmail.com>
2025-01-20 21:05:18 +0530

    20 th JAN

---
+++ src/Ex_01_JavaBasics/Lab_02_Comments.java
@@ -0,0 +1,7 @@
+package Ex_01_JavaBasics;
+
+public class Lab_02_Comments {
+//    Comment in Java programing language is the non executable part
+//    of the program.It helps only to increase the understandability of
+//    the program to the programmer.
+}
