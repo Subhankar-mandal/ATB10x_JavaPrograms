c14225fc8e7cbf7410e67e48e8c0ae8629122da4
Subhankar Mandal <subhankarmandal875@gmail.com>
2025-01-20 20:52:28 +0530

    20 th JAN

---
+++ src/Ex_01_JavaBasics/Lab_01_NoMainMethod.java
@@ -0,0 +1,9 @@
+package Ex_01_JavaBasics;
+
+public class Lab_01_NoMainMethod {
+//      There is no main method in the following program.So there is
+//    nothing to compile and interpret to get output.Here we have only
+//    class name.As we know program execution start from main method,and
+//    a class must contain a main method.Here's the program without having
+//    main method has no sense.
+}
