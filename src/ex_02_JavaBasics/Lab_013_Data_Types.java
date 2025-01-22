e8d0ae12dff32c185df7a58eaf2616e13c3b5cfd
Subhankar Mandal <subhankarmandal875@gmail.com>
2025-01-23 00:39:19 +0530

    20 th JAN

---
+++ src/ex_02_JavaBasics/Lab_013_Data_Types.java
@@ -0,0 +1,16 @@
+package ex_02_JavaBasics;
+
+public class Lab_013_Data_Types {
+    /* Data-type: It may be defined as the type of data (Such as int, char, float,
+                  boolean etc.).It has specific size that helps a variable to allocate
+                  that particular size in memory.
+                  */
+    public static void main(String[] args) {
+        boolean isGood = true;
+        byte b ;
+        short s ;
+        int a ;
+
+
+    }
+}
---
+++ src/ex_02_JavaBasics/Lab_014_Variables.java
@@ -0,0 +1,22 @@
+package ex_02_JavaBasics;
+
+public class Lab_014_Variables {
+    public static void main(String[] args) {
+        /**Variable: It may be defined as an identifier that is used to store
+         *            the value of any data depending on its type within memory
+         *            and those values can be changed at any instant of time within
+         *            the program.
+         *
+         *  Declaration Syntax: data_type variable_name = value;          */
+
+        boolean isEnergetic = true;
+        byte b = 127;
+        short s = 1234;
+        int i = 123456;
+        long mbNumber = 8250287607L;
+        char c = 'a';
+        float f = 99.99f;
+        double d = 96.8999999d;
+        System.out.println(mbNumber);
+    }
+}
